090cbe0e778c5117826ac14ebf46478e79b01ae0
duoxide <zauchka007@gmail.com>
2022-11-01 16:01:55 +0200

    second commit - come minor changes

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 # Instance creation
 
 resource "aws_instance" "alex-instance" {
-  count = var.instance_count
+  # count = var.instance_count
   ami                    = data.aws_ami.ubuntu_latest.id
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.main-public-1[0].id
@@ -9,6 +9,6 @@ resource "aws_instance" "alex-instance" {
   key_name               = aws_key_pair.mykeypair.key_name
   # user_data              = file("apache-b.sh")
   tags = {
-    Name = "alex-${count.index}"
+    Name = "alex-intance"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,11 @@
-output "instances_id" {
+output "instance_id" {
   description = "ID of the EC2 instance"
   value       = "${aws_instance.alex-instance.id}"
+  # value = "${aws_instance.alex-instance[0].id}, ${aws_instance.alex-instance[1].id}"
+}
+
+output "instance_public_ip" {
+  description = "ID of the EC2 instance"
+  value       = "${aws_instance.alex-instance.public_ip}"
   # value = "${aws_instance.alex-instance[0].id}, ${aws_instance.alex-instance[1].id}"
 }
